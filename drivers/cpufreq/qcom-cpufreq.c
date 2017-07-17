3eba20b585c698d5fbebc5a6509d7c0f948d47f9
Sultan Alsawaf <sultan@kerneltoast.com>
2017-07-16 19:46:59 -0700

    qcom-cpufreq: Use CLKFLAG_NO_RATE_CACHE
    After a CPU comes online, clk_set_rate() silently refuses to change said CPU's
    frequency to the frequency it was running at before going offline (since it
    thinks that we are setting the same frequency redundantly). By default, each CPU
    runs at its minimum frequency when coming online, so if the governor decides to
    keep a CPU running at the frequency it used before going offline, then
    clk_set_rate() will ignore the frequency change request and the CPU will stay
    stuck running at its minimum frequency for a potentially long period of time
    (i.e. until the governor decides to change the frequency to something
    different). This can cause severe lag when a device is woken up from deep sleep.
    
    In order to prevent a CPU from being stuck at its default frequency for a
    potentially long period of time, set the CLKFLAG_NO_RATE_CACHE flag so that the
    governor's frequency change requests will always be honored right after a CPU
    comes online.
    
    Note that this requires a CPU's component clocks to be using CLKFLAG_NO_RATE_CACHE
    as well in order to fix the issue.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -398,6 +398,8 @@ static int __init msm_cpufreq_probe(struct platform_device *pdev)
 		c = devm_clk_get(dev, clk_name);
 		if (IS_ERR(c))
 			return PTR_ERR(c);
+		else if (IS_ERR(c))
+			c = cpu_clk[cpu-1];
 		c->flags |= CLKFLAG_NO_RATE_CACHE;
 		cpu_clk[cpu] = c;
 	}
