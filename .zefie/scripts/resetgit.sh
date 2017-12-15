aa0a5141bfdd3917951d0e111ebacb1b667e9676
zefie <zefie@zefie.net>
2017-12-15 04:37:23 -0500

    zefie scripts: fix resetgit.sh not working properly

--- .zefie/scripts/resetgit.sh
+++ .zefie/scripts/resetgit.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-if [ $(ls -l AndroidKernel.mk | wc -l) -eq 1 ]; then
-	if [ $(git status | grep clean | wc -l) -ne 1 ]; then
-		if [ $(git status | grep "\.zefie/" | wc -l) -gt 0 ]; then
+if [ -d ".zefie" ]; then
+	if [ "$(git status | grep "modified:" | grep -v "\.zefie/" | wc -l)" -gt "0" ]; then
+		# If files other than those in .zefie have been modified, we shall reset
+		if [ "$(git status | grep "\.zefie/" | wc -l)" -gt "0" ]; then
+			# If we are resetting, and files in .zefie have been modified, we shall retain them
 			TMPFILE=$(tempfile -p zef- -s .zef)
+			echo "* Saving current status of zefie scripts..."
 			tar -cf ${TMPFILE} .zefie/
 		fi
 		rm * .config .config.old -rf
 		git reset --hard
 		if [ ! -z "${TMPFILE}" ]; then
 			rm -rf .zefie
+			echo "* Restoring pre-reset status of zefie scripts..."
 			tar -xf ${TMPFILE}
 			rm ${TMPFILE}
 		fi
