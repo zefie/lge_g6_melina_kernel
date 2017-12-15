265e8c3f79b0d2afd1cb5b5477139f0c5abfe52e
zefie <zefie@zefie.net>
2017-12-15 09:02:41 -0500

    Add DriveDroid CD-ROM Emulation support

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -3312,6 +3312,9 @@ static ssize_t file_store(struct device *dev, struct device_attribute *attr,
 static DEVICE_ATTR_RW(ro);
 static DEVICE_ATTR_RW(nofua);
 static DEVICE_ATTR_RW(file);
+#ifdef CONFIG_DRIVEDROID_CDROM
+DEVICE_ATTR(cdrom, 0644, fsg_show_cdrom_dd, fsg_store_cdrom_dd);
+#endif
 DEVICE_ATTR(perf, 0644, fsg_show_perf, fsg_store_perf);
 
 static struct device_attribute dev_attr_ro_cdrom = __ATTR_RO(ro);
@@ -3438,6 +3441,9 @@ EXPORT_SYMBOL_GPL(fsg_common_set_num_buffers);
 
 static inline void fsg_common_remove_sysfs(struct fsg_lun *lun)
 {
+#ifdef CONFIG_DRIVEDROID_CDROM
+	device_remove_file(&lun->dev, &dev_attr_cdrom);
+#endif
 	device_remove_file(&lun->dev, &dev_attr_nofua);
 	/*
 	 * device_remove_file() =>
@@ -3463,7 +3469,7 @@ static inline void fsg_common_remove_sysfs(struct fsg_lun *lun)
 	device_remove_file(&lun->dev, &dev_attr_ro);
 	device_remove_file(&lun->dev, &dev_attr_file);
 #ifdef CONFIG_LGE_USB_G_AUTORUN
-	device_remove_file(&lun->dev, &dev_attr_cdrom_usbmode);
+	device_remove_file(&lun->dev, &dev_attr_cdrom);
 #endif
 }
 
@@ -3612,6 +3618,12 @@ static inline int fsg_common_add_sysfs(struct fsg_common *common,
 	rc = device_create_file(&lun->dev, &dev_attr_nofua);
 	if (rc)
 		goto error;
+
+#ifdef CONFIG_DRIVEDROID_CDROM
+	rc = device_create_file(&lun->dev, &dev_attr_cdrom);
+	if (rc)
+		dev_err(&lun->dev, "failed to create sysfs entry: %d\n", rc);
+#endif
 
 #ifdef CONFIG_LGE_USB_G_AUTORUN
 	rc = device_create_file(&lun->dev, &dev_attr_cdrom_usbmode);
