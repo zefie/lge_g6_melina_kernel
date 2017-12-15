3e8034aca665d72e478da2b30f205e7436599835
zefie <zefie@zefie.net>
2017-12-15 03:23:59 -0500

    remove unused part of script

--- .zefie/scripts/prepare.sh
+++ .zefie/scripts/prepare.sh
@@ -5,7 +5,7 @@ DEFCONFIG_DIR="arch/${ARCH}/configs"
 cp -r .zefie/scripts/dtbTool scripts/
 
 IFS=$'\n'
-#CPU Specific Optimizations=cpu_opts.patch
+#patch_desc=file.patch
 read -r -d '' CUSTOM_LOCAL << EOM
 EOM
 
@@ -24,38 +24,9 @@ for c in ${CUSTOM_LOCAL}; do
 	IFS=${STDIFS}
 done;
 
-IFS=$'\n'
-read -r -d '' COMMITS << EOM
-EOM
-
-for c in ${COMMITS}; do
-	if [ ! -z "${c}" ]; then
-		IFS=
-		if [ -f ".zefie/patches/git/${c}.patch" ]; then
-			COMMIT_DATA=$(cat ".zefie/patches/git/${c}.patch")
-		else
-			COMMIT_DATA=$(curl -s -k "https://git.zefie.net/zefie/android_kernel_samsung_msm8916/commit/${c}.patch")
-			REMOTE=1
-		fi
-		PATCH_DATA=$(echo ${COMMIT_DATA} | sed -n '/^diff/,$p')
-		PATCH_INFO=$(echo ${COMMIT_DATA} | grep "^Subject" | cut -d' ' -f3-)
-		if [ -z "${REMOTE}" ]; then
-			echo "*** Applying local patch: ${PATCH_INFO}"
-		else
-			echo "*** Applying remote patch: ${PATCH_INFO}"
-		fi
-		echo ${PATCH_DATA} | patch -s -p1
-		RES=$?
-		if [ ${RES} -ne 0 ]; then
-			exit ${RES};
-		fi
-		IFS=${STDIFS}
-	fi
-done
-
 # Melina Kernel Custom Config Options
-#read -r -d '' CONFIG << EOM
-#EOM
+read -r -d '' CONFIG << EOM
+EOM
 
 if [ ! -z "${EXTRA_CONFIG}" ]; then
 	CONFIG+="$(echo; echo "${EXTRA_CONFIG}";)"
