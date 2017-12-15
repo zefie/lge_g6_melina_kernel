7dc4eaaf97e1e5ea93b39457862b6d2b7947357b
zefie <zefie@zefie.net>
2017-12-15 03:39:27 -0500

    zefie scripts: use ccache if enabled and available

--- .zefie/scripts/buildenv.sh
+++ .zefie/scripts/buildenv.sh
@@ -7,9 +7,27 @@ export KERNEL_NAME=melina
 export ANDROID_TARGET="LGE-Stock"
 export ARCH=arm64
 export CROSS_COMPILE="${TOOLCHAIN}"
+export Z_USE_CCACHE=1
+
+# Do not edit below this line
+
+if [ $(command -v ccache >/dev/null 2>&1) -eq $(true) ] && [ ${Z_USE_CCACHE} -eq 1 ]; then
+	if [ -z "${Z_CCACHE_NOTICE}" ]; then
+		export TOOLCHAIN="ccache ${TOOLCHAIN}"
+		echo " * zefie scripts notice: ccache enabled"
+		export Z_CCACHE_NOTICE=1
+	fi
+else
+	export TOOLCHAIN="/home/zefie/dev/toolchains/uber/out/aarch64-linux-android-6.x/bin/aarch64-linux-android-"
+	if [ -z "${Z_CCACHE_NOTICE}" ]; then
+		echo " * zefie scripts notice: ccache NOT enabled"
+		export Z_CCACHE_NOTICE=1
+	fi
+fi
 
 if [ "$(basename $0)" == "buildenv.sh" ]; then
 	if [ ! -z "$1" ]; then
 		$@
 	fi
 fi
+
