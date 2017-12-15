634d94c13139a6069000b564a15d0f841974e0e5
zefie <zefie@zefie.net>
2017-12-15 05:48:38 -0500

    include and use lz4demo, because current lz4 creates too new format

--- .zefie/scripts/prepare.sh
+++ .zefie/scripts/prepare.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source .zefie/scripts/buildenv.sh
+
 DEFCONFIG_DIR="arch/${ARCH}/configs"
 
 cp -r .zefie/scripts/dtbTool scripts/
@@ -6,6 +8,13 @@ cp -r .zefie/scripts/dtbTool scripts/
 # Melina Kernel Custom Config Options
 read -r -d '' CONFIG << EOM
 CONFIG_LOCALVERSION="-${KERNEL_NAME}"
+CONFIG_EXFAT_FS=m
+CONFIG_EXFAT_DISCARD=y
+CONFIG_EXFAT_DELAYED_SYNC=y
+CONFIG_EXFAT_KERNEL_DEBUG=n
+CONFIG_EXFAT_DEBUG_MSG=n
+CONFIG_EXFAT_DEFAULT_CODEPAGE=437
+CONFIG_EXFAT_DEFAULT_IOCHARSET="utf8
 EOM
 
 if [ ! -z "${EXTRA_CONFIG}" ]; then
@@ -19,3 +28,6 @@ cp -f ${DEFCONFIG_DIR}/${ORIG_DEFCONFIG} ${DEFCONFIG_DIR}/${KERNEL_DEFCONFIG}
 if [ ! -z "${CONFIG}" ]; then
 	echo "${CONFIG}" >> ${DEFCONFIG_DIR}/${KERNEL_DEFCONFIG}
 fi
+
+make -C .zefie/lz4demo clean > /dev/null 2>&1
+make -C .zefie/lz4demo
