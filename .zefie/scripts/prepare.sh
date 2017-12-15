2becd1eda4cd090450ceb36ff9e310eed498c286
zefie <zefie@zefie.net>
2017-12-15 04:52:15 -0500

    revert to proper kernel naming method

--- .zefie/scripts/prepare.sh
+++ .zefie/scripts/prepare.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
-STDIFS=${IFS}
 DEFCONFIG_DIR="arch/${ARCH}/configs"
 
 cp -r .zefie/scripts/dtbTool scripts/
 
-IFS=$'\n'
-#patch_desc=file.patch
-read -r -d '' CUSTOM_LOCAL << EOM
-EOM
-
-for c in ${CUSTOM_LOCAL}; do
-	IFS=
-	PATCH_INFO=$(echo "${c}" | cut -d'=' -f1)
-	PATCH_FILE=$(echo "${c}" | cut -d'=' -f2)
-	if [ -f ".zefie/patches/${PATCH_FILE}" ]; then
-		echo "*** Applying local patch: ${PATCH_INFO}"
-		patch -s -p1 < ".zefie/patches/${PATCH_FILE}";
-		RES=$?
-		if [ ${RES} -ne 0 ]; then
-			exit ${RES};
-		fi
-	fi
-	IFS=${STDIFS}
-done;
-
 # Melina Kernel Custom Config Options
 read -r -d '' CONFIG << EOM
-CONFIG_LOCALVERSION=""
+CONFIG_LOCALVERSION="-${KERNEL_NAME}"
 EOM
 
 if [ ! -z "${EXTRA_CONFIG}" ]; then
