3963badbbc7686aac53c84d24fac28819cd873e6
Evren Yurtesen <eyurtese@abo.fi>
2018-02-28 21:14:20 +0200

    Increase maximum charge input current
    Increase maximum charge input current from USB port using dedicated charging ports to 2A.
    All leeco msm8996 devices support QC3.0 which supports at least 9V 2A HVDCP charging.
    The USB Type-C port is rated for 3A power so it would be beneficial to use up to 2A current
    for both 5V and 9V charging. (in reality setting the values to 2000 results in 1870mA - 1960mA output
    from QC 3.0 chargers, tested on zl1 with 4 different charges using USB power meter)
    
    Change-Id: I924ad2bf8c17ea28c90cb3dc65e28e035398d6b5

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -723,7 +723,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_hvdcp_icl_ma = 1800;
+static int smbchg_default_hvdcp_icl_ma = 2000;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
@@ -735,7 +735,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 1800;
+static int smbchg_default_dcp_icl_ma = 2000;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
