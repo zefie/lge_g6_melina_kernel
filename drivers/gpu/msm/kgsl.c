f7b4764d7fde96d0abf0cbdf69e0cad9f6c5c595
mady51 <root@instance-1.c.thematic-gift-200703.internal>
2019-03-05 12:26:11 +0530

    high prio for kgsl

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4043,7 +4043,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 				PM_QOS_DEFAULT_VALUE);
 	}
 
-	device->events_wq = alloc_workqueue("kgsl-events",
+	device->events_wq = alloc_workqueue("kgsl-events", WQ_HIGHPRI |
 		WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_SYSFS, 0);
 
 	/* Initalize the snapshot engine */
