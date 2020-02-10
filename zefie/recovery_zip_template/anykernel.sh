92149228a46d141d3bec53c719b7db72fcaf933d
zefie <zefie@zefie.net>
2020-02-10 03:08:33 -0500

    update to anykernel3

--- zefie/recovery_zip_template/anykernel.sh
+++ zefie/recovery_zip_template/anykernel.sh
@@ -1,11 +1,13 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# EDIFY properties
+# begin properties
+properties() { '
 kernel.string="%NAME% kernel for %MANU% %MODEL% by %KERNELDEV%"
 do.devicecheck=1
-do.modules=0 # we will do it ourself.
+do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=%MODEL_WHITELIST%
@@ -13,17 +15,50 @@ device.name2=
 device.name3=
 device.name4=
 device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
-force_seandroid=1;
+ramdisk_compression=auto;
+
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
 
-## zefie kernel info
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+#backup_file init.rc;
+#replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+# init.tuna.rc
+#backup_file init.tuna.rc;
+#insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.lucye" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+#append_file init.tuna.rc "bootscript" init.tuna;
+
+# fstab.lucye
+#backup_file fstab.lucye;
+#patch_fstab fstab.lucye /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+#patch_fstab fstab.lucye /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+#patch_fstab fstab.lucye /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+#append_file fstab.lucye "usbdisk" fstab;
+
+# end ramdisk changes
+
 
 ui_print " "
 ui_print "Kernel Device: %MANU% %MODEL% (%DEVMODEL%)"
@@ -33,54 +68,18 @@ ui_print "Kernel Version: %VERSION%"
 ui_print "Toolchain: %TOOLCHAIN_VERSION%"
 ui_print " "
 
-## AnyKernel install
-
-# Dump initrd
-dump_boot;
-
-# remove /sbin/rctd if it exists
-if [ -f "$ramdisk/sbin/rctd" ]; then
-	PATCHED=1
-	ui_print "Removing /sbin/rctd..."
-	rm -f "$ramdisk/sbin/rctd"
-fi
-
-if [ -f "$ramdisk/init.lge.rc" ]; then
-	if [ "$(grep /sbin/rctd $ramdisk/init.lge.rc | wc -l)" -gt "0" ]; then
-		PATCHED=1
-		ui_print "Removing rctd service from init.lge.rc..."
-		sed -ie '/\/sbin\/rctd/,+4d' "$ramdisk/init.lge.rc"
-	fi
-fi
-
-if [ -f "$ramdisk/fstab.lucye" ]; then
-	if [ "$(grep forceencrypt $ramdisk/fstab.lucye | wc -l)" -gt "0" ]; then
-		PATCHED=1
-		ui_print "Disabling forceencrypt..."
-		sed -ie 's/forceencrypt/encryptable/' "$ramdisk/fstab.lucye"
-	fi
-fi
-
-if [ -f "$ramdisk/init.rc" ]; then
-	if [ "$(grep /system/bin/install-recovery $ramdisk/init.rc | wc -l)" -gt "0" ]; then
-		PATCHED=1
-		ui_print "Removing custom recovery killer..."
-		sed -ie '/\/system\/bin\/install-recovery/,+2d' "$ramdisk/init.rc"
-	fi
-fi
-
-# write new boot
 write_boot;
+
 
 # Manual module install
 ui_print "Installing modules...";
 if `mount -o rw,remount -t auto /vendor`; then
 	ui_print "Detected Android Vendor Partition"
 	MODDIR=/vendor/lib/modules
-elif `mount -o rw,remount -t auto /system`; then
+else
 	if [ -d "/system/vendor/lib/modules" ]; then
 		ui_print "Detected Android System-Root with Vendor-on-System"
-		MODDIR=/system/vendor/lib/modules
+		MODDIR=/system_root/vendor/lib/modules
 	elif [ -d "/system/system/vendor/lib/modules" ]; then
 		ui_print "Detected Android System-Root with nested Vendor"
 		MODDIR=/system/system/vendor/lib/modules
@@ -88,7 +87,11 @@ elif `mount -o rw,remount -t auto /system`; then
 		ui_print "Detected Android System-Root without Vendor"
 		MODDIR=/system/system/lib/modules
 	elif [ -d "/system/lib/modules" ]; then
-		ui_print "Detected Classic Android System"
+		if [ -d "/system/system" ]; then
+			ui_print "Detected Android System-Root with Root Modules"
+		else
+			ui_print "Detected Classic Android System"
+		fi
 		MODDIR=/system/lib/modules
 	fi
 fi
@@ -104,17 +107,12 @@ else
 fi
 
 
-#if `mount -o rw,remount -t auto /data`; then
-#	if [ -f /data/adb/magisk/addon.d.sh ]; then
-#	  ui_print "Detected Magisk install, attempting to restore Magisk"
-#	  exec sh /data/adb/magisk/addon.d.sh "\$@"
-#	fi
-#fi
+ui_print "******************************************"
+ui_print "If you find this release useful, please"
+ui_print "consider supporting zefie on Patreon"
+ui_print " "
+ui_print "http://zef.pw/patreon"
+ui_print "******************************************"
 
-# remount any partitions we mounted rw as ro, except /data
-for f in system vendor; do
-	if [ $(df | grep -c "/$f") -gt 0 ]; then
-		mount -o ro,remount -t auto "/$f";
-	fi
-done
 ## end install
+
