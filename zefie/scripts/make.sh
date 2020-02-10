1ba9670f55367517c8d14e597e1860da98829cea
zefie <zefie@zefie.net>
2020-02-09 23:09:53 -0500

    fix ccache build

--- zefie/scripts/make.sh
+++ zefie/scripts/make.sh
@@ -5,4 +5,8 @@ SCRIPTDIR=$(realpath "$(dirname "${0}")")
 source "${SCRIPTDIR}/buildenv.sh"
 
 errchk mkdir -p "${KERNEL_BUILD_DIR}"
-errchk make O="${KERNEL_BUILD_DIR}" "${@}"
+if [ -z "${USE_CCACHE}" ]; then
+	errchk make O="${KERNEL_BUILD_DIR}" CROSS_COMPILE="${TOOLCHAIN}" CROSS_COMPILE_ARM32="${TOOLCHAIN32}" CC="ccache gcc" "${@}"
+else
+	errchk make O="${KERNEL_BUILD_DIR}" CROSS_COMPILE="${TOOLCHAIN}" CROSS_COMPILE_ARM32="${TOOLCHAIN32}" "${@}"
+fi
