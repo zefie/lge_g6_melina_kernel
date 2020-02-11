b0948d70c3898e4421e1c77f07d07a0c14eced6d
zefie <zefie@zefie.net>
2020-02-11 15:25:11 -0500

    add debug build option to testbuild

--- zefie/scripts/defconfig.sh
+++ zefie/scripts/defconfig.sh
@@ -5,7 +5,7 @@ SCRIPTDIR=$(realpath "$(dirname "${0}")")
 source "${SCRIPTDIR}/buildenv.sh"
 
 if [ ! -f "${DEFCONFIG_DIR}/${KERNEL_NAME_LOWER}_${KERNEL_DEVMODEL_LOWER}_defconfig" ]; then
-	errchk "${SCRIPTDIR}/create_defconfigs.sh"
+	errchk "${SCRIPTDIR}/create_defconfigs.sh" "${Z_CONFIG_OVERRIDE[@]}"
 fi
 DEFCONFIG="${KERNEL_NAME_LOWER}_${KERNEL_DEVMODEL_LOWER}_defconfig"
 echo "Using ${DEFCONFIG}..."
--- zefie/scripts/testbuild.sh
+++ zefie/scripts/testbuild.sh
@@ -15,23 +15,60 @@ errchk cd "${KERNEL_SOURCE_DIR}"
 Z_BUILD_SCRIPT="${SCRIPTDIR}/build.sh"
 Z_BUILD_SCRIPT_PARAM=()
 
+Z_CONFIG_OVERRIDE=()
 
-if [ "${1}" == "clean" ]; then
-	shift
-	errchk rm -rf "${KERNEL_SOURCE_DIR:?}/${KERNEL_BUILD_DIR:?}"
-	errchk "${SCRIPTDIR}/clean.sh"
-elif [ "${1}" == "prodtest" ]; then
-	shift
-	export FORCE_RESET=1
-	errchk "${SCRIPTDIR}/clean.sh"
-	Z_BUILD_SCRIPT="${SCRIPTDIR}/do_kernel_build.sh"
-	Z_BUILD_SCRIPT_PARAM=("${KERNEL_DEVMODEL}")
-elif [ -z "${1}" ]; then
-	Z_BUILD_SCRIPT="${SCRIPTDIR}/build.sh"
-	Z_BUILD_SCRIPT_PARAM=("${@}")
-fi
+while [ "${1}" != "" ]; do
+                case "${1}" in
+			"build")
+				shift
+				errchk "${SCRIPTDIR}/create_defconfigs.sh" "${Z_CONFIG_OVERRIDE[@]}"
+				errchk "${SCRIPTDIR}/defconfig.sh"
+				errchk "${Z_BUILD_SCRIPT}" "${Z_BUILD_SCRIPT_PARAM[@]}"
+				;;
 
+			"zip")
+				shift
+				errchk "${SCRIPTDIR}/buildzip.sh"
+				;;
 
-errchk "${SCRIPTDIR}/create_defconfigs.sh" CONFIG_MELINA_WARN_AS_ERR=y
-errchk "${SCRIPTDIR}/defconfig.sh"
-errchk "${Z_BUILD_SCRIPT}" "${Z_BUILD_SCRIPT_PARAM[@]}"
+			"clean")
+				shift
+				errchk rm -rf "${KERNEL_SOURCE_DIR:?}/${KERNEL_BUILD_DIR:?}"
+				errchk "${SCRIPTDIR}/clean.sh"
+				;;
+
+			"gitreset")
+				shift
+				errchk rm -rf "${KERNEL_SOURCE_DIR:?}/${KERNEL_BUILD_DIR:?}"
+				export FORCE_RESET=1
+				errchk "${SCRIPTDIR}/clean.sh"
+				export FORCE_RESET=
+				;;
+
+			"prodtest")
+				shift
+				"${0}" gitreset
+				Z_BUILD_SCRIPT="${SCRIPTDIR}/do_kernel_build.sh"
+				Z_BUILD_SCRIPT_PARAM=("${KERNEL_DEVMODEL}")
+				"${Z_BUILD_SCRIPT}" "${Z_BUILD_SCRIPT_PARAM[@]}"
+				exit $?
+				;;
+
+			"debug")
+				Z_CONFIG_OVERRIDE+=("CONFIG_MELINA_ENHANCED_NDBG=n" "CONFIG_MELINA_DEBUG_ENABLE=y")
+				export Z_CONFIG_OVERRIDE
+				shift
+				;;
+
+			"clang")
+				USE_CLANG=1
+				export USE_CLANG
+				shift
+				;;
+			*)
+				Z_BUILD_SCRIPT_PARAM+=("${1}")
+				shift
+				;;
+	esac
+done
+
