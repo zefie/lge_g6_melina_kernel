4ec4ef085031b6d2f93bd63f49c5b6ae23d08d2e
zefie <zefie@zefie.net>
2020-02-14 14:45:48 -0500

    update scripts for more models (TEST)

--- zefie/scripts/buildenv.sh
+++ zefie/scripts/buildenv.sh
@@ -51,10 +51,10 @@ export DEFAULT_MANU="LG" # for zip filename
 export DEFAULT_MODEL="G6" # for zip filename
 export DEFAULT_DEVMODEL="US997" # for zip filename, and anykernel whitelist (converted to lowercase for whitelist)
 
-
 # Do not edit below this line
 
-export SUPPORTED_MODELS=("US997" "H870" "H872")
+#export SUPPORTED_MODELS=("US997" "H870" "H872")
+SUPPORTED_MODELS=($(grep -A100 '# start model list' "${SCRIPTDIR}/functions.sh" | grep -B100 '# end model list' | grep ')$' |  grep -v '(' | grep -v "\*" | grep -v "\-\-" | cut -d'"' -f2))
 export PATH="${PWD}/zefie/lz4demo:${PATH}"
 export ARCH="arm64"
 export DEFCONFIG_DIR="arch/${ARCH}/configs"
@@ -69,7 +69,9 @@ if [ -z "${TC_TYPE}" ]; then export TC_TYPE="${ZEFIE_TC_TYPE}"; fi
 if [ -z "${TC_VER}" ]; then export TC_VER="${ZEFIE_TC_VER}"; fi
 if [ -z "${KERNEL_MANU}" ]; then export KERNEL_MANU="${DEFAULT_MANU}"; fi
 if [ -z "${KERNEL_MODEL}" ]; then export KERNEL_MODEL="${DEFAULT_MODEL}"; fi
-if [ -z "${KERNEL_DEVMODEL}" ]; then kernel_setdevice "${DEFAULT_DEVMODEL}"; fi
+if [ -z "${KERNEL_DEVMODEL}" ]; then KERNEL_DEVMODEL="${DEFAULT_DEVMODEL}"; fi
+
+kernel_setdevice "${KERNEL_DEVMODEL}"
 
 if [ ! -z "${USE_CCACHE}" ]; then
 	TOOLCHAIN="ccache ${TOOLCHAIN}"
