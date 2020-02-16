72d393060d5ae023df2e252e1bed85031fab6cbf
zefie <zefie@zefie.net>
2020-02-16 15:04:52 -0500

    qdsp6v2: compilability

--- drivers/misc/qcom/qdsp6v2/audio_amrwbplus.c
+++ drivers/misc/qcom/qdsp6v2/audio_amrwbplus.c
@@ -104,7 +104,7 @@ static long audio_ioctl_shared(struct file *file, unsigned int cmd,
 		pr_debug("%s:AUDIO_START sessionid[%d]enable[%d]\n", __func__,
 			audio->ac->session,
 			audio->enabled);
-		if (audio->stopped == 1)
+		if (audio->stopped == 1) {
 			audio->stopped = 0;
 			break;
 		}
